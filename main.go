59a7493e40f9193c659db579fa475bf08a24dccb
postatum <postatum@gmail.com>
2013-11-16 16:50:00 +0200

    Applied gofmt

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "lets-go/views"
+	"lets-go/views"
+	"net/http"
 )
 
 func ConnectHandlers() {
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
-    http.HandleFunc("/api/people", views.PeopleResource)
-    http.HandleFunc("/api/people/add", views.PersonAddResource)
-    http.HandleFunc("/", views.PeopleView)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.HandleFunc("/api/people", views.PeopleResource)
+	http.HandleFunc("/api/people/add", views.PersonAddResource)
+	http.HandleFunc("/", views.PeopleView)
 }
 
 func main() {
-    ConnectHandlers()
-    http.ListenAndServe(":8000", nil)
+	ConnectHandlers()
+	http.ListenAndServe(":8000", nil)
 }
--- views/views.go
+++ views/views.go
@@ -1,67 +1,66 @@
 package views
 
 import (
-    "fmt"
-    "net/http"
-    "labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
-    "encoding/json"
-    "html/template"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"net/http"
 )
 
 const (
-    mongourl = "127.0.0.1:27017"
-    mongodb = "godb"
+	mongourl   = "127.0.0.1:27017"
+	dbname     = "godb"
+	collection = "people"
+	PER_PAGE   = 5
 )
-
 
 type JsonResponse map[string]interface{}
 
 func (r JsonResponse) String() (s string) {
-    b, err := json.Marshal(r)
-    if err != nil {
-        return ""
-    }
-    return string(b)
+	b, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(b)
 }
 
-
 func PeopleResource(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	session, _ := mgo.Dial(mongourl)
+	defer session.Close()
+	collection := session.DB(dbname).C(collection)
 
-    session, _ := mgo.Dial(mongourl)
-    defer session.Close()
-    collection := session.DB(mongodb).C("people")
+	people := make([]map[string]interface{}, PER_PAGE)
+	_ = collection.Find(bson.M{}).Sort("-_id").Limit(PER_PAGE).All(&people)
 
-    people := make([]map[string]interface{}, 5)
-    _ = collection.Find(bson.M{}).Sort("-_id").Limit(5).All(&people)
-
-    fmt.Fprint(w, JsonResponse{"people": people})
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JsonResponse{"people": people})
 }
 
 func PersonAddResource(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	person := make(map[string]interface{})
+	person["name"] = r.FormValue("name")
+	person["email"] = r.FormValue("email")
 
-    person := make(map[string]interface{})
-    person["name"] = r.FormValue("name")
-    person["email"] = r.FormValue("email")
+	session, _ := mgo.Dial(mongourl)
+	defer session.Close()
+	collection := session.DB(dbname).C(collection)
 
-    session, _ := mgo.Dial(mongourl)
-    defer session.Close()
-    collection := session.DB(mongodb).C("people")
+	if err := collection.Insert(&person); err != nil {
+		fmt.Fprint(w, JsonResponse{"err": err.Error()})
+		return
+	}
 
-    if err := collection.Insert(&person); err != nil {
-        fmt.Fprint(w, JsonResponse{"err": err.Error()})
-        return
-    }
-    fmt.Fprint(w, JsonResponse{"success": "true"})
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JsonResponse{"success": "true"})
 }
 
 func PeopleView(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-type", "text/html")
-    t, _ := template.ParseFiles("templates/index.html")
+	t, _ := template.ParseFiles("templates/index.html")
 
-    context := make(map[string]interface{})
+	context := make(map[string]interface{})
 
-    t.Execute(w, &context)
+	w.Header().Set("Content-type", "text/html")
+	t.Execute(w, &context)
 }
